6d59085f426e2150fa2fad31ee79df8e411deb54
bezvozni <prostoprostor@gmail.com>
2023-04-18 08:36:44 +0300

    4 0% 'nest new ..' - (.spec & test) + swagger = init

---
+++ 4/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('start swagg')
+    .setDescription('the first description about this API')
+    .setVersion('1.0')
+    .addTag('whatIsTag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
